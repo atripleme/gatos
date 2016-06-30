e890c641679af8bcfe0d67928bcd7ca8fd0eb35a
Mario Moreno <malejo279@gmail.com>
2016-06-30 09:52:34 -0500

    Creación del modelo gatos

--- gatos/models.py
+++ gatos/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Gato (models.Model):
+    name = models.CharField(max_length=255)
+    patas = models.ImageField(default=4)
