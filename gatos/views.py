2629806dd72b53728ef95fc902c64e25a581fc99
Mario Moreno <malejo279@gmail.com>
2016-06-30 10:08:40 -0500

    Crear la vista para mostrar gatos

--- gatos/views.py
+++ gatos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
+from .models import Gato
 # Create your views here.
+
+def gato(request):
+    gatos = Gato.objects.order_by('id')
+    template = loader.get_template('gato.html')
+    title = 'Los gatos son cool'
+    context = {
+        'gatos' = gatos,
+        'title' = title
+    }
+    return HttpResponse(template.render(context, request))
